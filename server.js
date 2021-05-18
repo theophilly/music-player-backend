7a4e2790bbf1b020eeacaf06ebc7d956222d0d1f
theophilly <theophilly20@gmail.com>
2021-05-17 21:56:22 -0700

    api

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 import Music from './models/MusicModel.js';
+
+const app = express();
+
+dotenv.config();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,7 +22,26 @@ mongoose
   .then(() => console.log('database conection succesfully'))
   .catch((err) => console.log(err));
 
-const app = express();
+app.post('/addmusic', async (req, res) => {
+  const { name, artist, path, img } = req.body;
+
+  try {
+    let music = await new Music({ name, artist, path, img });
+    music = await music.save();
+    return res.status(200).json(music);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get('/allmusic', async (req, res) => {
+  try {
+    const allMusic = await Music.find({});
+    return res.send(200).json(allMusic);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('app running on port 5000'));
