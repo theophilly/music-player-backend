fd6686221877564814619fc4ab4d3751d9915fb4
kokoruns <kokoruns@gmail.com>
2022-12-01 19:08:39 -0800

    include rsvps

---
+++ models/RsvpModel.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const rsvpSchema = new mongoose.Schema({
+  full_name: {
+    type: String,
+  },
+  email_address: {
+    type: String,
+  },
+  phone: {
+    type: Number,
+  },
+});
+
+export default mongoose.model("Rsvp", rsvpSchema);
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import Music from './models/MusicModel.js';
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import Music from "./models/MusicModel.js";
+import Rsvp from "./models/RsvpModel.js";
 
 const app = express();
 
@@ -16,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const corsOptions = {
-  origin: '*',
+  origin: "*",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
@@ -27,10 +28,10 @@ mongoose
     `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.egm3f.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
   )
-  .then(() => console.log('database conection succesfully'))
+  .then(() => console.log("database conection succesfully"))
   .catch((err) => console.log(err));
 
-app.post('/addmusic', async (req, res) => {
+app.post("/addmusic", async (req, res) => {
   const { name, artist, path, img } = req.body;
 
   try {
@@ -42,7 +43,7 @@ app.post('/addmusic', async (req, res) => {
   }
 });
 
-app.get('/allmusic', async (req, res) => {
+app.get("/allmusic", async (req, res) => {
   try {
     const allMusic = await Music.find({});
 
@@ -52,5 +53,42 @@ app.get('/allmusic', async (req, res) => {
   }
 });
 
+app.post("/addrsvp", async (req, res) => {
+  const { full_name, email_address, phone } = req.body;
+
+  try {
+    let rsvp = await new Rsvp({ full_name, email_address, phone });
+    rsvp = await rsvp.save();
+    return res.status(200).json(rsvp);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/allrsvps", async (req, res) => {
+  try {
+    const allRsvps = await Rsvp.find({});
+
+    return res.status(200).json(allRsvps);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.delete("/delrsvp", async (req, res) => {
+  if (!req.query.id.match(/^[0-9a-fA-F]{24}$/)) {
+    return res.status(404).json({ message: "Rsvp not found" });
+  }
+
+  const rsvp = await Rsvp.findById(req.query.id);
+
+  if (rsvp) {
+    await rsvp.remove();
+    return res.json({ message: "Rsvp removed successfully" });
+  } else {
+    return res.status(404).json({ message: "Rsvp not found" });
+  }
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('app running on port 5000'));
+app.listen(PORT, () => console.log("app running on port 5000"));
